03decfa8133a61dada3b26f0a5bf78b80de3f88f
yaoyuhui <435607052@qq.com>
2020-03-11 21:56:30 +0800

    一级路由、404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+// import { Button } from 'antd-mobile';
+import { BrowserRouter as Router,Route,Link,Switch } from 'react-router-dom';
+import Home from './pages/Home'
+import CityList from './pages/CityList'
+import Map from './pages/Map'
+import NotFount from './pages/NotFount';
 
 function App() {
   return (
-    <div className="App">
-    app
-    </div>
+    <Router className="App">
+      <Link to="/home">首页</Link>
+      <Link to="/cityList">城市列表</Link>
+      <Link to="/map">地图</Link>
+      <Switch>
+      {/*一级路由*/}
+      <Route path="/home" component={Home}></Route>
+      <Route path="/cityList" component={CityList}></Route>
+      <Route path="/map" component={Map}></Route>
+
+      <Route component={NotFount}></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/NotFount/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const NotFount=()=><center><h1>404...</h1></center>
+
+
+
+
+export default NotFount
